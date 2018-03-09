7f8cda5e11509264bb3903de59f779491b318376
hui.he <hui.he@oakridge.io>
2018-03-09 15:26:31 +0800

    change nav

--- oakops-front-end/src/routes.js
+++ oakops-front-end/src/routes.js
@@ -27,44 +27,31 @@ let routes = [
     {
         path: '/',
         component: Home,
-        name: '导航一',
-        iconCls: 'el-icon-message',//图标样式class
-        children: [
-            { path: '/main', component: Main, name: '主页', hidden: true },
-            { path: '/table', component: Table, name: 'Table' },
-            { path: '/form', component: Form, name: 'Form' },
-            { path: '/user', component: user, name: '列表' },
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: '导航二',
-        iconCls: 'fa fa-id-card-o',
-        children: [
-            { path: '/page4', component: Page4, name: '页面4' },
-            { path: '/page5', component: Page5, name: '页面5' }
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
         name: '',
-        iconCls: 'fa fa-address-card',
+        iconCls: 'fa fa-bar-chart',
         leaf: true,//只有一个节点
         children: [
-            { path: '/page6', component: Page6, name: '导航三' }
+            { path: '/echarts', component: echarts, name: '首页' }
         ]
-    },
+    },    
     {
         path: '/',
         component: Home,
-        name: 'Charts',
-        iconCls: 'fa fa-bar-chart',
+        name: '企业站点管理',
+        iconCls: 'fa fa-id-card-o',
         children: [
-            { path: '/echarts', component: echarts, name: 'echarts' }
+            { path: '/page4', component: Page4, name: '站点列表' }        
         ]
     },
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: 'Charts',
+    //     iconCls: 'fa fa-bar-chart',
+    //     children: [
+    //         { path: '/echarts', component: echarts, name: 'echarts' }
+    //     ]
+    // },
     {
         path: '*',
         hidden: true,
