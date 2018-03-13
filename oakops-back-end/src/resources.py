a412010baafa684955901ccc3f095282eeffabb5
hui.he <hui.he@oakridge.io>
2018-03-13 16:21:27 +0800

    add jwt

---
+++ oakops-back-end/src/resources.py
@@ -0,0 +1,70 @@
+from flask import jsonify
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+
+parser = reqparse.RequestParser()
+parser.add_argument('username', help='This field cannot be null', required=True)
+parser.add_argument('password', help='This field cannot be null', required=True)
+
+blacklist = set()
+
+class RevokedTokenModel():        
+
+    @classmethod
+    def is_jti_blacklisted(self, jti):
+        return jti in blacklist
+
+    @classmethod
+    def addToBlacklist(self, jti):
+        blacklist.add(jti)
+
+    @classmethod
+    def getBlackList(self):
+        return blacklist
+
+
+class UserLogin(Resource):
+    def post(self):
+        data = parser.parse_args()
+        try:
+            if data['username'] != 'hui.he@oakridge.io' or data['password'] != 'oakridge':
+                return jsonify({"msg": "Bad username or password"}), 401
+
+            return {
+                'message' : 'Login success',             
+                'access_token': create_access_token(identity = data['username']),
+                'refresh_token': create_refresh_token(identity = data['username'])
+            }
+
+            # return jsonify(ret), 200
+
+        except:
+            return {
+                 'message' : 'Something went wrong'
+            }, 500
+
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        RevokedTokenModel.addToBlacklist(jti)
+        return {"msg": "Successfully logged out"}
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        ret = {
+            'access_token': create_access_token(identity=current_user)
+        }
+        return jsonify(ret), 200            
+
+
+
+# Example: The protected resuource need token
+class SecretResource(Resource):
+    @jwt_required
+    def get(self):
+        return {
+            'answer': 42
+        }           
---
+++ oakops-back-end/src/run.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+api = Api(app)
+
+import views, resources
+
+jwt = JWTManager(app)
+# JWT Configuration
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-oakridge'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return resources.RevokedTokenModel.is_jti_blacklisted(jti)
+
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogout, '/logout')
+api.add_resource(resources.SecretResource, '/my')
+api.add_resource(resources.TokenRefresh, '/tokenrefresh')
