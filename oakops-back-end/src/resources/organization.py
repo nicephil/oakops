7a7f1b6cfc76177135e7eb590dbd58e57cada9b2
XiaoYin <xiaoyin.xia@oakridge.io>
2018-03-15 14:09:40 +0800

    Site list coding.

--- oakops-back-end/src/resources/organization.py
+++ oakops-back-end/src/resources/organization.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import (jwt_required, jwt_refresh_token_required)
 import requests
+from flask import request
 
 cloud_agent_root_url='http://cloud.oakridge.io/agent/cloud/v1'
 
@@ -12,5 +13,11 @@ class OrganizationList(Resource):
 
     # @jwt_required
     def get(self):
-        res = requests.get(cloud_agent_root_url + "/organizations")
+        url = cloud_agent_root_url + "/organizations/sites"
+        full_path = request.full_path
+        index = full_path.find('?')
+        if index != -1:
+            url = url + full_path[index:]
+        print(url)
+        res = requests.get(url)
         return res.json()
